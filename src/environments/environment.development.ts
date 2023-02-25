15087f8efa92018d3a0d354fd75bf8278ddea744
gustavo <gustavo@stallos.com.br>
2023-02-25 11:31:15 -0300

    warn added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,3 +2,6 @@ export const environment = {
   openWeatherApiKey: '{OpenWeatherApiKey}',
   openWeatherBaseURL: 'https://api.openweathermap.org/data/2.5/weather'
 };
+
+// check in your openWeather account and replace {OpenWeatherApiKey} for you API Key:
+// https://home.openweathermap.org/api_keys
