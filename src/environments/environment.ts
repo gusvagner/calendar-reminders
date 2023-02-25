5b6fb392b0530e89ae98a8546dab05a9bd74330a
gustavo <gustavo@stallos.com.br>
2023-02-25 11:28:59 -0300

    removing api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  openWeatherApiKey: '5ab921b26b487200e80c629a02c890e3',
+  openWeatherApiKey: '{OpenWeatherApiKey}',
   openWeatherBaseURL: 'https://api.openweathermap.org/data/2.5/weather'
 };
